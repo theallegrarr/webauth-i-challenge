dc344664244b48ece148cfc1ed9ef6e5769ce60c
theallegrarr <nurudeen.amedu@gmail.com>
2019-11-11 12:34:33 +0100

    bugs fixed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 
-const db = require('./database/dbConfig.js');
+const db = require('./data/dbConfig.js');
 const Users = require('./users/users-model.js');
 
 const server = express();
